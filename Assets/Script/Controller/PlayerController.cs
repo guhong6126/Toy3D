2ededc607515330d02ddcd91f0995ec5ec5145f6
guhong <redcircle12@naver.com>
2024-07-31 18:48:49 +0900

    .

--- Assets/Script/Controller/PlayerController.cs
+++ Assets/Script/Controller/PlayerController.cs
@@ -55,6 +55,11 @@ public class PlayerController : MonoBehaviour
         anim.SetFloat("speed", _speed);
     }
 
+    void OnRunEvent()
+    {
+        Debug.Log("ddobug");
+    }
+
     void UpdateIdle()
     {
         Animator anim = GetComponent<Animator>();
