a1f0ffddf1ff798814b9f76e1578148911b03a74
guhong <redcircle12@naver.com>
2024-08-01 22:53:30 +0900

    UI, Utils

--- Assets/Script/Utils/Define.cs
+++ Assets/Script/Utils/Define.cs
@@ -1,14 +1,21 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Define
 {
+    public enum UIEvent
+    {
+        Click,
+        Drag,
+    }
+
     public enum MouseEvent
     {
         Press,
         Click,
     }
+
     public enum CameraMode
     {
         QuarterView,
